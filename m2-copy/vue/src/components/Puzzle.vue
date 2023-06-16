9e6f49a9820371e8c32884554346ad3a8c7c9ecb
Taylor Peoples <mtaylorpeoples@gmail.com>
2023-06-16 13:04:25 -0400

    Created empty input grid for crossword puzzle

--- m2-copy/vue/src/components/Puzzle.vue
+++ m2-copy/vue/src/components/Puzzle.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="puzzle-container">
     <div class="clues">
-  
       <ol>
-        <li v-for="crossword in crosswords" v-bind:key="crossword.id">{{ crossword.word}}</li>
-        <li>clue one</li>
+        <li v-for="crossword in crosswords" v-bind:key="crossword.id">
+          {{ crossword.word }}
+        </li>
+        <li>
+          clue one more text here to test out auto spacing of clues container
+        </li>
         <li>clue two</li>
       </ol>
     </div>
-    <div class="crossword">empty crossword</div>
+    <div class="crossword">
+      empty crossword
+      <form class="parent-grid"></form>
+    </div>
   </div>
 </template>
 
@@ -19,13 +25,29 @@ export default {
   data() {
     return {
       crosswords: [],
-    }
+    };
   },
-  methods: {},
+  methods: {
+    cellSelected() {
+      let cellClicked = document.querySelector(".lettercell");
+      cellClicked.se;
+    },
+  },
   created() {
     CrosswordsService.listWords().then((response) => {
       this.crosswords = response.data;
     });
+  },
+  mounted() {
+    let cell = document.querySelector(".parent-grid");
+    for (let i = 1; i <= 625; i++) {
+      let child = document.createElement("input");
+      child.className = "lettercell";
+      child.setAttribute("maxlength", "1");
+      child.setAttribute("style", "text-transform:uppercase");
+
+      cell.append(child);
+    }
   },
 };
 </script>
@@ -33,9 +55,9 @@ export default {
 <style>
 .puzzle-container {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr auto auto;
   grid-template-rows: 1fr 1fr 1fr;
-  gap: 0px 0px;
+  gap: 0px;
   grid-auto-flow: row;
   grid-template-areas:
     "clues crossword crossword"
@@ -45,10 +67,35 @@ export default {
 
 .clues {
   grid-area: clues;
+  margin-right: 25px;
 }
 
 .crossword {
   grid-area: crossword;
-  
+  margin: 10px;
+}
+
+.parent-grid {
+  display: grid;
+  grid-template-rows: repeat(25, auto);
+  grid-template-columns: repeat(25, auto);
+  border: 2px;
+  border-color: black;
+  border-style: solid;
+}
+
+.lettercell {
+  border: 1px;
+  border-style: solid;
+  border-color: black;
+  padding: 4px;
+  height: 40px;
+  width: 40px;
+  font-size: xx-large;
+  text-align: center;
+}
+
+input:focus {
+  background: lightgoldenrodyellow;
 }
 </style>
